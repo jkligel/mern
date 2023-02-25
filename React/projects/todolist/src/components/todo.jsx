fad0256a1b6dda6a351e91c2f09e18d0541aac9d
Joseph Kligel <jkligel@gmail.com>
2023-02-24 19:57:25 -0600

    todolist project

---
+++ React/projects/todolist/src/components/todo.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import '../App.css'
+
+const Todo = (props) => {
+    
+    const [newTodo, setNewTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    return(
+        <p>{ }</p>
+    )
+} 
+
+export default Todo;
